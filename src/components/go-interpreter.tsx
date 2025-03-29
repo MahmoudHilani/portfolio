6b219ae40561834548f421eeed80ed50812e9410
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2025-03-29 23:15:56 +0300

    fix: terminal UI (WIP)

--- src/components/go-interpreter.tsx
+++ src/components/go-interpreter.tsx
@@ -10,20 +10,18 @@ const GoTerminal: React.FC = () => {
   const [history, setHistory] = useState<string[]>([]);
   const [historyIndex, setHistoryIndex] = useState(-1);
   const [isLoading, setIsLoading] = useState(false);
+  const [isFocused, setIsFocused] = useState(true)
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!input.trim() || isLoading) return;
-
+    const curInput = input 
     setHistory([...history, input]);
     setHistoryIndex(-1);
-
-    setOutput([...output, `${input}\n`]);
+    setInput("")
+    setOutput([...output, `${curInput}\n`]);
     setIsLoading(true);
-
-    const payload = JSON.stringify({ "code": input });
-   console.log("Sending payload:", payload);
 
     try {
       const response = await fetch(API_URL, {
@@ -31,15 +29,12 @@ const GoTerminal: React.FC = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ code: input }),
+        body: JSON.stringify({ code: curInput }),
       });
 
       const data = await response.json();
       console.log(data);
-
-      if (data.error) {
-        setOutput((prev) => [...prev, `Error: ${data.error}`]);
-      } else {
+      if (data.result != "No result") {
         setOutput((prev) => [...prev, data.result]);
       }
     } catch (error) {
@@ -47,6 +42,7 @@ const GoTerminal: React.FC = () => {
     } finally {
       setIsLoading(false);
       setInput("");
+      setTimeout(() => inputRef.current?.focus(), 1);
     }
   };
 
@@ -77,21 +73,27 @@ const GoTerminal: React.FC = () => {
       <div className="terminal body">
         {output.map((line, index) => (
           <div key={index} className="terminal line">
-            {line}
+            {">>"} {line}
           </div>
         ))}
       </div>
       <form onSubmit={handleSubmit}>
+        <span>{">> "}</span>
         <input
           ref={inputRef}
           type="text"
           value={input}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={handleKeyDown}
-          className="terminal input"
+          className="terminal input focus:outline-none max-w-full"
           disabled={isLoading}
           autoFocus
         ></input>
+        {isFocused &&
+          <span className="h-5 w-3 bg-gray-400"></span> 
+        }
       </form>
     </div>
   );
