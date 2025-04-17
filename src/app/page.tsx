b9a57c40a2597f29517d22c6280a92dfe1e4d5e5
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2025-04-17 23:11:22 +0300

    feat: hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { motion } from "motion/react";
 import Image from "next/image";
+import { IconArrowDown } from "@tabler/icons-react";
 
 export default function Home() {
   return (
@@ -15,16 +16,22 @@ export default function Home() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="relative flex flex-col gap-4 w-full h-full "
+        className="relative flex flex-col gap-4 w-full h-full px-13"
       >
-        <div className="text-3xl md:text-[172px] tracking-tighter dark:text-white">
-          Mahmoud Hilani
+        <div className="flex text-3xl absolute -top-7 left-9 lg:text-[120px] xl:text-[176px] tracking-tighter gap-6">
+          <p className="dark:text-white ">Mahmoud</p>
+          <p className="text-white mix-blend-exclusion">Hilani</p>
         </div>
-        <div className="font-extralight text-xl md:text-4xl dark:text-neutral-200 px-3">
-          Software Engineer
+
+        <div className="flex justify-end font-light text-xl md:text-4xl dark:text-neutral-200 mr-40 mt-52">
+          <p className="w-md">Setting the standard for good development. Here to learn, here to improve.</p>
         </div>
-        
+        <div className="flex items-center text-xl md:text-4xl dark:text-neutral-200 ml-40 mt-86 gap-2">
+          <p className="">What I've been up to</p>
+            <IconArrowDown size={36} className="animate-bounce"/>
+        </div>
       </motion.div>
     </AuroraBackground>
+    
   );
 }
